5123e94dd5297425d308a6b19f91cd93f156fd61
Ayush Tripathi <ayushdev635@gmail.com>
2026-09-13 11:55:55 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+import random
+
+while True:
+    choice = input("Roll the dice? (yes/no): ").lower()
+
+    if choice == "yes":
+        dice = random.randint(1, 6)
+        print("🎲 You rolled:", dice)
+
+    elif choice == "no":
+        print("Thanks for playing! 👋")
+        break
+
+    else:
+        print("Please enter yes or no.")
